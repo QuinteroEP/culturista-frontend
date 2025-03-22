d1081ea7b4f1c0d745d3a98e855366ef4f22ce0a
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-03-22 16:23:43 -0500

    v1
    Servicios implementados

---
+++ culturista/src/app/infodestino/infodestino.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infodestino',
+  imports: [],
+  templateUrl: './infodestino.component.html',
+  styleUrl: './infodestino.component.css'
+})
+export class InfodestinoComponent {
+
+}
