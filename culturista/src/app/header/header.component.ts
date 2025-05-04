3200f0d29dfc0e2aa2dfe45b733807174d3bc367
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2025-05-03 22:10:04 -0500

    v4.1
    CSS

--- culturista/src/app/header/header.component.ts
+++ culturista/src/app/header/header.component.ts
@@ -1,38 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../service/authService';
-import { accountService } from 'src/app/service/accountService';
+import { accountService } from '../service/accountService';
 import { planService } from '../service/planService';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-
 export class HeaderComponent {
-  constructor(private router: Router, private auth: AuthService, private accountService: accountService, private plan: planService) { }
-
-  greeting: String  = '';
+  greeting: string = '';
   logged: boolean = false;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private accountService: accountService,
+    private plan: planService,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
 
   ngOnInit() {
     this.auth.logged$.subscribe(value => {
       this.logged = value;
-      if (this.auth.email !== '') {
-        this.accountService.findClientByEmail(this.auth.email).subscribe(
-          (usuario) => {
-            this.greeting = 'Hola ' + usuario.nombre;
-            this.plan.name = usuario.nombre;
-          }
-        );
-      }
-      else{
+      if (this.auth.email) {
+        this.accountService.findClientByEmail(this.auth.email).subscribe(user => {
+          this.greeting = 'Hola ' + user.nombre;
+          this.plan.name = user.nombre;
+        });
+      } else {
         this.greeting = 'Hola John Doe';
       }
     });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const headerEl = this.el.nativeElement.querySelector('header');
+    if (window.pageYOffset > 50) {
+      this.renderer.addClass(headerEl, 'scrolled');
+    } else {
+      this.renderer.removeClass(headerEl, 'scrolled');
+    }
   }
 
   goToHome() {
