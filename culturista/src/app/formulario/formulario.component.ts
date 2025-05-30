939696771b9e538558124dd6460c48da28f6fbce
Pablo Enrique Quintero Callamand <pabloenriquequintero@gmail.com>
2025-05-30 11:43:27 -0500

    Revert "v4.2.1"
    This reverts commit 88682b8dca640e1b3ff6f0bcc0b9f3546b2d8380.

--- culturista/src/app/formulario/formulario.component.ts
+++ culturista/src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
@@ -10,8 +10,7 @@ import { tipoActividad } from '../entity/tipoActividad';
   selector: 'app-formulario',
   imports: [FormsModule],
   templateUrl: './formulario.component.html',
-  styleUrl: './formulario.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
   listaTipos!: tipoActividad[];
